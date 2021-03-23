74bc5aac57d85dbe6f3988c8062cf4a4e4a38e06
A-atmos <aavashchhetri01@gmail.com>
2021-03-23 23:16:40 +0545

    Rearranging Columns

--- pokemon_data_write.py
+++ pokemon_data_write.py
@@ -5,5 +5,17 @@ df = pd.read_csv('pokemon_data.csv')
 # print(df.head(5))
 
 df['Total'] = df['HP']+df['Attack'] + df['Sp. Atk']+ df['Sp. Def'] + df['Speed']
-print(df.head(5))
-print(df['Total'])
+# print(df.head(5))
+
+'''# another method
+df['Total'] = df.iloc[:,4:10].sum(axis=1)'''
+
+# df = df.drop(columns = ['Total']) -- drop to clear/delete the column
+# print(df.head(5))
+
+cols = list(df.columns.values)
+print(cols)
+df = df[cols[0:4]+[cols[-1]]+cols[4:len(cols)-1]] #since cols[-1] will be a string, [cols[-1]] is as list
+print(df.head(3))
+
+# print(df['Total'])
