4bc08e8a17df9b09b4e6778b44cb79378650b7f9
A-atmos <aavashchhetri01@gmail.com>
2021-03-23 22:41:27 +0545

    Adding Total

--- pokemon_data_write.py
+++ pokemon_data_write.py
@@ -2,4 +2,8 @@ import numpy as np
 import pandas as pd
 
 df = pd.read_csv('pokemon_data.csv')
+# print(df.head(5))
+
+df['Total'] = df['HP']+df['Attack'] + df['Sp. Atk']+ df['Sp. Def'] + df['Speed']
 print(df.head(5))
+# print(df['Total'])
